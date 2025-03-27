b39928a76eaa573ab6093f2f9f953c6263991fb2
Toni Latorre <alatorre08@boscdelacoma.cat>
2025-03-27 09:27:15 +0100

    Update Main.java

--- UNO/Main.java
+++ UNO/Main.java
@@ -1,9 +1,22 @@
 package UNO;
+
 public class Main {
     public static void main(String[] args) {
-        Mazo mazo = new Mazo();
+        Carta c1 = new Carta(1, Color.Groc);
+        Carta c2 = new Carta(3, Color.Vermell);
+        Carta c3 = new Carta(6, Color.Blau);
+        Carta c4 = new Carta(9, Color.Verd);
 
-        System.out.println("barrejar mazo:");
-        mazo.mostrarMazo();
+        Jugador j1 = new Jugador("Jugador 1");
+        j1.addCarta(c1);
+        j1.addCarta(c2);
+        j1.addCarta(c3);
+        j1.addCarta(c4);
+
+        for (Carta c : j1.getCartes()) {
+            UI.mostrarCarta(c);
+        }
+
+        UI.mostrarCartes(j1.getCartes());
     }
 }
