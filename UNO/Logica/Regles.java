150c16cff5b09b237e813382e9c521b58363c85c
Toni Latorre <alatorre08@boscdelacoma.cat>
2025-04-25 10:40:23 +0200

    Regles actualitzades

--- UNO/Logica/Regles.java
+++ UNO/Logica/Regles.java
@@ -1,4 +1,8 @@
 package uno.Logica;
 
 public class Regles {
+    public static boolean sonCartesCompatibles(Carta c1, Carta c2) {
+        return c1.getColor() == c2.getColor() || c1.getNumero() == c2.getNumero();
+    }
 }
+
