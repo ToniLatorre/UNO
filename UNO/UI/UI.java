cc3d11db8888c16e4f71f64e6043b802cf3473d4
Toni Latorre <alatorre08@boscdelacoma.cat>
2025-05-13 11:14:42 +0200

    Update UI.java

--- UNO/UI/UI.java
+++ UNO/UI/UI.java
@@ -2,6 +2,7 @@ package uno.UI;
 
 import java.util.*;
 import uno.Logica.Carta;
+import uno.Logica.TipusCarta;
 
 public class UI {
     // ANSI escape codes
@@ -10,44 +11,86 @@ public class UI {
     public static final String GREEN = "\u001B[32m";
     public static final String YELLOW = "\u001B[33m";
     public static final String BLUE = "\u001B[34m";
+    public static final String WHITE = "\u001B[37m";
 
     private static String pintarCarta(Carta carta) {
-        String color = "";
-        switch (carta.getColor()) {
-            case Groc:
-                color = YELLOW;
-                break;
-            case Vermell:
-                color = RED;
-                break;
-            case Blau:
-                color = BLUE;
-                break;
-            case Verd:
-                color = GREEN;
-                break;
-            default:
-                break;
+        String color = WHITE;
+
+        if (carta.getColor() != null) {
+            switch (carta.getColor()) {
+                case Groc:
+                    color = YELLOW;
+                    break;
+                case Vermell:
+                    color = RED;
+                    break;
+                case Blau:
+                    color = BLUE;
+                    break;
+                case Verd:
+                    color = GREEN;
+                    break;
+            }
         }
 
-        String cartaPintada = String.format("""
-            %s┌─────────┐%s
-            %s│ %d       │%s
-            %s│         │%s
-            %s│   UNO   │%s
-            %s│         │%s
-            %s│       %d │%s
-            %s└─────────┘%s""",
-                color, RESET,
-                color, carta.getNumero(), RESET,
-                color, RESET,
-                color, RESET,
-                color, RESET,
-                color, carta.getNumero(), RESET,
-                color, RESET);
+        String cartaText;
+        if (carta.getTipus() == TipusCarta.NORMAL) {
+            cartaText = String.format("""
+                %s┌─────────┐%s
+                %s│ %d       │%s
+                %s│         │%s
+                %s│   UNO   │%s
+                %s│         │%s
+                %s│       %d │%s
+                %s└─────────┘%s""",
+                    color, RESET,
+                    color, carta.getNumero(), RESET,
+                    color, RESET,
+                    color, RESET,
+                    color, RESET,
+                    color, carta.getNumero(), RESET,
+                    color, RESET);
+        } else {
+            // Special card display
+            String simbol = "";
+            switch (carta.getTipus()) {
+                case Prohibit:
+                    simbol = "✕";
+                    break;
+                case CanviSentit:
+                    simbol = "->";
+                    break;
+                case Mes2:
+                    simbol = "+2";
+                    break;
+                case CanviColor:
+                    simbol = "[]";
+                    break;
+                case Mes4:
+                    simbol = "+4";
+                    break;
+                default:
+                    simbol = "?";
+            }
 
+            cartaText = String.format("""
+                %s┌─────────┐%s
+                %s│ %s       │%s
+                %s│         │%s
+                %s│   %s   │%s
+                %s│         │%s
+                %s│       %s │%s
+                %s└─────────┘%s""",
+                    color, RESET,
+                    color, simbol, RESET,
+                    color, RESET,
+                    color, simbol, RESET,
+                    color, RESET,
+                    color, simbol, RESET,
+                    color, RESET);
+        }
 
-        return cartaPintada;
+        return cartaText;
     }
 
     public static void mostrarCarta(Carta carta) {
@@ -76,5 +119,4 @@ public class UI {
         }
         System.out.println();
     }
-
 }
